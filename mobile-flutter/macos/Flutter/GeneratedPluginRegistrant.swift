d254ed70f241ed7c4223f4f65e38f11603339353
Vivek Verma <myselfvivek9@gmail.com>
2026-05-29 12:58:21 +0530

    Update flutter dependencies

--- mobile-flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile-flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,10 @@ import Foundation
 
 import file_selector_macos
 import flutter_contacts
-import flutter_secure_storage_macos
+import flutter_secure_storage_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterContactsPlugin.register(with: registry.registrar(forPlugin: "FlutterContactsPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
 }
